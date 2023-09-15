6cf14d7a7b269739070be5622172d562c78b08d0
AdityaOT <143172434+AdityaOT@users.noreply.github.com>
2023-09-15 17:59:12 +0530

    Update Logger.h

--- Pod/Classes/Logger.h
+++ Pod/Classes/Logger.h
@@ -6,18 +6,18 @@
 //  Copyright (c) 2015 Pierre-Marc Airoldi. All rights reserved.
 //
 
-#ifndef Swig_Logger_h
-#define Swig_Logger_h
+// #ifndef Swig_Logger_h
+// #define Swig_Logger_h
 
-#import <CocoaLumberjack/CocoaLumberjack.h>
+// #import <CocoaLumberjack/CocoaLumberjack.h>
 
-#undef LOG_LEVEL_DEF
-#define LOG_LEVEL_DEF myLibLogLevel
+// #undef LOG_LEVEL_DEF
+// #define LOG_LEVEL_DEF myLibLogLevel
 
-#ifdef DEBUG
-__unused static const int myLibLogLevel = DDLogLevelDebug;
-#else
-__unused static const int myLibLogLevel = DDLogLevelOff;
-#endif
+// #ifdef DEBUG
+// __unused static const int myLibLogLevel = DDLogLevelDebug;
+// #else
+// __unused static const int myLibLogLevel = DDLogLevelOff;
+// #endif
 
-#endif
+// #endif
