fe7923276ae22923a268c25c65158519a765dd8f
AdityaOT <143172434+AdityaOT@users.noreply.github.com>
2023-09-15 18:07:02 +0530

    Update transport_adapter_nack.c

--- Pod/Classes/TransportAdapter/transport_adapter_nack.c
+++ Pod/Classes/TransportAdapter/transport_adapter_nack.c
@@ -520,7 +520,7 @@ static pj_status_t transport_attach(pjmedia_transport *tp,
     
     /* Get pointer RTP session information of the media stream */
     if (a->stream_type == PJMEDIA_TYPE_VIDEO)
-        pjmedia_vid_stream_get_rtp_session_tx(a->stream_ref, &a->rtp_tx_ses);
+        // pjmedia_vid_stream_get_rtp_session_tx(a->stream_ref, &a->rtp_tx_ses);
     /* Not implemented */
     else
         return PJ_EINVALIDOP;
